85c0d5fce683bb4a95be732d4e2196b7d8838474
pwmiller <miller1417@gmail.com>
2015-10-16 15:18:23 -0400

    Removed a lot of the excess, trimmed down the number of templates; ready to begin prototyping the demo.html

--- application/views.py
+++ application/views.py
@@ -4,12 +4,25 @@ from .app import app
 @app.route('/')
 def home():
     return render_template('home.html')
-
+    
 @app.route('/demo')
 def demo():
     return render_template('demo.html')
-    
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
+    
+# @app.route('/contact')
+# def about():
+    # return render_template('contact_us.html')
+    
+# @app.route('/faq')
+# def about():
+    # return render_template('faq.html')
+    
+# @app.route('/about')
+# def about():
+    # return render_template('about.html')
+    
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404

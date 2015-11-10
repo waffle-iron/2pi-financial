d7dd9ff0f5fe24294d99833f6f260d36c82535ec
pwmiller <miller1417@gmail.com>
2015-11-10 10:59:08 -0500

    Preparation for Heroku deploy

--- config.py
+++ config.py
@@ -11,18 +11,13 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 
-# https://pwmiller@pwmiller1.scm.azurewebsites.net:443/pwmiller1.git
-# git remote add azure https://pwmiller@pwmiller1.scm.azurewebsites.net:443/pwmiller1.git
-# git push azure master
+# HEROUKU
+# miller1417@gmail.com : OiV4T3Zw$A&8
 
-# Data Source=tcp:c2shlbm7o1.database.windows.net,1433;Initial Catalog=pwmilleAXuLcuAf3;User Id=proddb@c2shlbm7o1;Password=qG$1tSGnYYq0;
-# pwmiller1
-# pwmilleAXuLcuAf3
-# proddb:qG$1tSGnYYq0
+# heroku login
+Run `heroku create --stack cedar`
+Run `heroku addons:add shared-database`
+Run `git push heroku master`
 
-
-# AZURE GIT
-# pwmiller : VZfj0!vyh2hm
-
-# AZURE FTP
-# pwmiller1\$pwmiller1 : fCnuNayH0rtipi3qHAN1eHXbbLk8dYabpf6A5NB9KCXLQo9XiKWyNLttEzQG
+PATH=/usr/local/bin:/usr/bin:/cygdrive/c/users/patrick.miller/appdata/local/enthought/canopy/user/scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Lib:/usr/local/bin:/usr/bin:/cygdrive/c/Rtools/bin:/cygdrive/c/Rtools/gcc-4.6.3/bin:/cygdrive/d/app/client/patrick.miller/product/12.1.0/client_1/bin:/cygdrive/d/app/client/patrick.miller/product/12.1.0/client_1:/cygdrive/c/ProgramData/Oracle/Java/javapath:/cygdrive/c/Windows/system32:/cygdrive/c/Windows:/cygdrive/c/Windows/System32/Wbem:/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0:/cygdrive/c/Program Files (x86)/Common Files/Ulead Systems/MPEG:/cygdrive/c/Program Files/Intel/DMIX:/cygdrive/c/Program Files (x86)/QuickTime/QTSystem:/cygdrive/d/Autoinjestion:/cygdrive/c/Program Files/MiKTeX 2.9/miktex/bin/x64:/cygdrive/c/program files/java/jdk1.8.0_45/bin:/cygdrive/c/MinGW/bin:/usr/bin:/cygdrive/c/Program Files (x86)/Windows Kits/8.1/Windows Performance Toolkit:/cygdrive/c/Program Files/Microsoft SQL Server/110/Tools/Binn:/cygdrive/c/Program Files (x86)/Microsoft SDKs/TypeScript/1.0:/cygdrive/c/Program Files/Microsoft SQL Server/120/Tools/Binn:/cygdrive/c/mingw/bin:/cygdrive/c/Program Files/MiKTeX 2.9/miktex/bin/x64/:/cygdrive/c/program files/java/jdk1.8.0_45/bin:/cygdrive/c/RBuildTools/3.1/bin:/cygdrive/c/Program Files (x86)/Heroku/bin:/cygdrive/c/Program Files (x86)/git/cmd:/cygdrive/c/users/patrick.miller/appdata/local/enthought/canopy/user/scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Lib:/cygdrive/c/mingw/bin:/cygdrive/c/Program Files/MiKTeX 2.9/miktex/bin/x64/:/cygdrive/c/program files/java/jdk1.8.0_45/bin:/cygdrive/c/RBuildTools/3.1/bin
+HerokuPath=C:\Program Files (x86)\Heroku

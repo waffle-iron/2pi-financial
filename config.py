46f56df1f68518859b8191538f244460420abfa9
pwmiller <miller1417@gmail.com>
2015-11-10 11:26:19 -0500

    Attempt to get an app pushed to Heroku

--- config.py
+++ config.py
@@ -15,9 +15,5 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 # miller1417@gmail.com : OiV4T3Zw$A&8
 
 # heroku login
-Run `heroku create --stack cedar`
-Run `heroku addons:add shared-database`
-Run `git push heroku master`
-
-PATH=/usr/local/bin:/usr/bin:/cygdrive/c/users/patrick.miller/appdata/local/enthought/canopy/user/scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Lib:/usr/local/bin:/usr/bin:/cygdrive/c/Rtools/bin:/cygdrive/c/Rtools/gcc-4.6.3/bin:/cygdrive/d/app/client/patrick.miller/product/12.1.0/client_1/bin:/cygdrive/d/app/client/patrick.miller/product/12.1.0/client_1:/cygdrive/c/ProgramData/Oracle/Java/javapath:/cygdrive/c/Windows/system32:/cygdrive/c/Windows:/cygdrive/c/Windows/System32/Wbem:/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0:/cygdrive/c/Program Files (x86)/Common Files/Ulead Systems/MPEG:/cygdrive/c/Program Files/Intel/DMIX:/cygdrive/c/Program Files (x86)/QuickTime/QTSystem:/cygdrive/d/Autoinjestion:/cygdrive/c/Program Files/MiKTeX 2.9/miktex/bin/x64:/cygdrive/c/program files/java/jdk1.8.0_45/bin:/cygdrive/c/MinGW/bin:/usr/bin:/cygdrive/c/Program Files (x86)/Windows Kits/8.1/Windows Performance Toolkit:/cygdrive/c/Program Files/Microsoft SQL Server/110/Tools/Binn:/cygdrive/c/Program Files (x86)/Microsoft SDKs/TypeScript/1.0:/cygdrive/c/Program Files/Microsoft SQL Server/120/Tools/Binn:/cygdrive/c/mingw/bin:/cygdrive/c/Program Files/MiKTeX 2.9/miktex/bin/x64/:/cygdrive/c/program files/java/jdk1.8.0_45/bin:/cygdrive/c/RBuildTools/3.1/bin:/cygdrive/c/Program Files (x86)/Heroku/bin:/cygdrive/c/Program Files (x86)/git/cmd:/cygdrive/c/users/patrick.miller/appdata/local/enthought/canopy/user/scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Scripts:/cygdrive/c/Users/patrick.miller/AppData/Local/Enthought/Canopy/User/Lib:/cygdrive/c/mingw/bin:/cygdrive/c/Program Files/MiKTeX 2.9/miktex/bin/x64/:/cygdrive/c/program files/java/jdk1.8.0_45/bin:/cygdrive/c/RBuildTools/3.1/bin
-HerokuPath=C:\Program Files (x86)\Heroku
+# heroku create
+# git push heroku master`

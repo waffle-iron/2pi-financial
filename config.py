a59ae7007c24a64f9b7787bc2629a652be905d3d
pwmiller <miller1417@gmail.com>
2016-08-16 12:55:15 -0400

    Update risk test page to include basic template code

--- config.py
+++ config.py
@@ -9,8 +9,10 @@ ENV = os.getenv('FLASKENV', 'prod')
 
 if ENV == 'development':
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:1111/development'
-else:
+elif ENV == 'development':
     SQLALCHEMY_DATABASE_URI = 'postgres://mmcogvvfdbnpor:rmXMarJXrh6Sh3mKkRO7SQpCnY@ec2-54-197-245-93.compute-1.amazonaws.com:5432/damnjigp8qp428'
-
+else:
+    raise Exception('No SQL database URI specified')
+    
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
